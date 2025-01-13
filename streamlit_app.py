ef6b65abad124ede68ec567f6bc0106e70a583f5
shadowmourne <62612619+nagamov8584@users.noreply.github.com>
2025-01-13 12:19:28 +0000

    modified:   streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,15 @@ def env_initiation():
         st.session_state.recognized_files = []
     if 'nonrecognized_files' not in st.session_state:
         st.session_state.nonrecognized_files = []
-def data_upload(db):
+def file_segregation(data):
+    # Function separates uploaded data into 3 structures:
+
+    # initial - raw_files
+    #_________________________________
+    # generated - recognized_files
+    # generated - nonrecognized_files
+    # generated - uploaded_files (all)
+
     file_not_detected = '❌ Файл не определен ❌'
     account_not_detected = '❌ Номер счета не найден ❌'
 
@@ -48,39 +56,38 @@ def data_upload(db):
             if if_found:
                 account_detected = if_found[0]
             # Second check: if in Spreadsheet DB
-                if account_detected in db.account.tolist():
+                if account_detected in data.account.tolist():
                     file.name = account_detected
                     if file_name not in extract(st.session_state.recognized_files):
                         st.session_state.recognized_files.append([file_name, account_detected, True])
                         st.session_state.uploaded_files.append(([file_name, account_detected, True]))
                 
                 else:
-                    st.session_state.nonrecognized_files.append([file_name, account_not_detected, False])
-                    st.session_state.uploaded_files.append(([file_name, account_not_detected, False]))
+                    if file_name not in extract(st.session_state.nonrecognized_files):
+                        st.session_state.nonrecognized_files.append([file_name, account_not_detected, False])
+                        st.session_state.uploaded_files.append(([file_name, account_not_detected, False]))
             else:
                 if file_name not in extract(st.session_state.nonrecognized_files):
                     st.session_state.nonrecognized_files.append([file_name, file_not_detected, False])
                     st.session_state.uploaded_files.append(([file_name, file_not_detected, False]))
-def db_creation(statements, db):
-    upload_db = pd.DataFrame(statements, columns=['upl_filename', 'account', 'recognition_status'])
-
+def db_creation(data, db):
+    upload_db = pd.DataFrame(data, columns=['upl_filename', 'account', 'recognition_status'])
     rename_db = upload_db.merge(db ,how='left', on='account')[
         ['account', 'upl_filename', 'file_name', 'class']]
     
     return upload_db, rename_db
-def recongnition_status(upload, statements):
+def recongnition_status(upload, data):
     col1, col2, col3 = st.columns(3)
     #
     upl_m = len(upload)
-    recogn_m = len(statements)
+    recogn_m = len(data)
     nonrecogn_m = upl_m - recogn_m
     #
     col1.metric(label="Файлов загружено", value=upl_m, delta=upl_m, delta_color='off')
     col2.metric("Распознано", recogn_m, recogn_m)
     col3.metric("Нераспознано", nonrecogn_m, nonrecogn_m, delta_color='inverse')
 def preview():
-
-
+    
     if_show_rename = st.toggle('Show upload&rename table')
     if if_show_rename and not len(upload) == 0:
         options = {
@@ -97,7 +104,6 @@ def preview():
         
     elif if_show_rename and len(upload) == 0:
         st.warning('No files uploaded')
-
 def create_zip(upload, db):
         zip_buffer = io.BytesIO()
         
@@ -116,9 +122,6 @@ def create_zip(upload, db):
         
         zip_buffer.seek(0)  # Rewind the buffer to the beginning
         return zip_buffer
-
-
-
 def look_at_fund_accounts (db):
     option = st.radio("Choose AMC of interest", options=["None", "S+", "WIM"], index=0)
 
@@ -131,28 +134,8 @@ def look_at_fund_accounts (db):
     ###
 def download_zip(upload, db):
     if st.button('Download ZIP of PDFs'):
-
-
-
-
-
-
-
-
-
-
         if upload:
             zip_buffer = create_zip(upload, db)
-
-
-
-
-
-
-
-
-
-            
             st.download_button(
                 label="Download ZIP",
                 data=zip_buffer,
@@ -171,13 +154,13 @@ upload = st.file_uploader(
     "Загружаем файлики", accept_multiple_files=True, type='pdf')
 
 #statements = []
-statements = data_upload(db=data)
-upload_db, rename_db = db_creation(statements=st.session_state.recognized_files, db=data)
+file_segregation(data=data)
+
+upload_db, rename_db = db_creation(data=st.session_state.recognized_files, db=data)
 #
 add_vertical_space(6)
 #
-recongnition_status(upload, statements=st.session_state.recognized_files)
-
+recongnition_status(upload, data=st.session_state.recognized_files)
 preview()
 #
 add_vertical_space(6)
